beb3a0da454b4877259ed4e55eb67058f65bdbff
autodistries <automatedactions@protonmail.com>
2022-01-05 00:47:04 +0100

    Code quality greatly updated thanks to Ven. Will add thanks but im too tired now

--- TimestampUtilities/build.gradle.kts
+++ TimestampUtilities/build.gradle.kts
@@ -1,9 +1,10 @@
-version = "1.0.1"
-description = "Adds /userinfo userID and /timestamp ID"
+version = "1.0.0"
+description = "Adds a /whois that return the most info it can find about your user"
 
-aliucord.changelog.set("""
+/*aliucord.changelog.set("""
     # 1.0.1
     * Added /userinfo command, which takes timestamp to get any user's info. Will add a way to know if you have a server in common with the guy later.
     * Bettered /timestamp
     *Code cleanup but there is more to do
 """.trimIndent())
+*/
