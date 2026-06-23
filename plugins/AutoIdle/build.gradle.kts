e74f92dc914f252ddf694be11805797b7c8ada7a
autodistries <66792425+autodistries@users.noreply.github.com>
2026-06-23 23:34:19 +0200

    bump ver

--- plugins/AutoIdle/build.gradle.kts
+++ plugins/AutoIdle/build.gradle.kts
@@ -1,9 +1,12 @@
-version = "1.0.0" // Plugin version. Increment this to trigger an update
+version = "1.1.0" // Plugin version. Increment this to trigger an update
 description = "Marks you idle when unfocusing Aliucord, restores your status on focus like RN does (only if you were online before)"
 
 aliucord {
     changelog.set(
         """
+        # 1.1.0
+        * fix keeping your status when autoidling
+        * reduce qty of false positives
         # 1.0.0
         * Initial plugin release!
         """.trimIndent(),
